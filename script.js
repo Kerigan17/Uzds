fac6305000bb641060410ed5c0442a582a6c1b9f
Kerigan17 <104769289+Kerigan17@users.noreply.github.com>
2023-04-06 15:18:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -50,12 +50,15 @@ let bigRat = $('#bigRat');
 let mouseOne = $('#mouse-one');
 let mouseTwo = $('#mouse-two');
 let mouseThree = $('#mouse-three');
+let windowXscroll = $(window).scrollTop();
+
+bigRat[0].src = '../images/rats/bigRat.png';
+bigRat.css('top', windowXscroll + 460);
 
 $(window).scroll(function() {
-    let windowXscroll = $(window).scrollTop();
+    windowXscroll = $(window).scrollTop();
     if (windowXscroll < 1430) {
         bigRat.removeClass('ratRun');
-        bigRat[0].src = 'src/images/bigRat.png';
         bigRat.css('top', windowXscroll + 460);
     } else {
         bigRat.addClass('ratRun');
@@ -70,20 +73,8 @@ $(window).scroll(function() {
         mouseThree.css('bottom', 65+windowXscroll/2);
         mouseThree.css('right', 73-windowXscroll/2);
     }
-
-
-
-
 });
-
-function ratAnimation() {
-
-}
 
 bigRat.click(function (){
     bigRat.src = 'ratAnimation/animation1.png'
 })
-
-
-
-
